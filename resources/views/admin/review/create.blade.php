373237beb77d60efea1ff2ae56589cb566600953
Ryosuke <spit1211@icloud.com>
2019-10-06 05:45:24 +0000

    submit

---
+++ resources/views/admin/review/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+
+@section('title', 'Reviewの新規作成')
+
+@section('content')
+ <div class="container">
+     <div class="row">
+         <div class="col-md-8 mx-auto">
+             <h2>Reviewの新規作成</h2>
+         </div>
+     </div>
+ </div>
+ @endsection
+ 
